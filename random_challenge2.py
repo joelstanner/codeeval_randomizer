79629eadfb6bcfbcf113609dca345dada9f2c73d
Joel Stanner <joel@joelstanner.com>
2015-10-13 15:37:54 -0700

    added some documentation

--- random_challenge2.py
+++ random_challenge2.py
@@ -1,4 +1,18 @@
-"""some code by user aperezalbela http://dpaste.com/0957WEF"""
+"""some code by user aperezalbela http://dpaste.com/0957WEF
+
+To use this script, go to https://www.codeeval.com/open_challenges/ and save
+the html file there to 'codeeval_open_challenges.html' (you must be logged in).
+Now, in the same directory that you saved that HTML file, run this script with
+a number corresponding to which level of eval you are looking to attempt. For
+example 'python random_challenge2.py 1'. This will return a random challenge
+from the "easy" category. The valid choices are as follows:
+
+1 - easy
+2 - moderate
+3 - hard
+4 - any
+
+"""
 
 import ast
 import random
